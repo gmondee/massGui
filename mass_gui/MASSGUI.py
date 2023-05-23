92da935906a56b4b67a74d9cb4e2d259cc83603c
gmondee <gmondee@g.clemson.edu>
2023-05-23 12:01:58 -0600

    First commit

---
+++ mass_gui/MASSGUI.py
@@ -0,0 +1,12 @@
+import PyQt5.QtWidgets as qt 
+
+
+def main():
+    app = qt.QApplication([])
+    label = qt.QLabel("Hello")
+    label.show()
+
+    #button.clicked.connect(on_button_clicked)
+
+    app.exec_()
+main()
---
+++ mass_gui/__init__.py
@@ -0,0 +1,3 @@
+#import pieces of the GUI package here if you might want to use them externally. For example, "import mass_gui.MASSGUI" or something
+
+from .MASSGUI import main
